d3205682811a97051a2e3647939f5bd59c62434a
Steeva06 <steeva0620@gmail.com>
2025-05-06 17:15:45 +0530

    Create TestConstants.java

---
+++ src/main/java/com/epam/training/student_Joseph Steve Waugh_Tummapudi/webdriver_task3/testdata/TestConstants.java
@@ -0,0 +1,15 @@
+package com.epam.training.webdriver_task3.testdata;
+
+public interface TestConstants {
+    String TEXT_TO_SEARCH = "Google Cloud Platform Pricing Calculator";
+    int NUMBER_OF_INSTANCES = 4;
+    String OPERATING_SYSTEM = "Free: Debian, CentOS, CoreOS, Ubuntu or BYOL (Bring Your Own License)";
+    String PROVISIONING_MODEL = "Regular";
+    String SERIES = "N1";
+    String MACHINE_TYPE = "n1-standard-8, vCPUs: 8, RAM: 30 GB";
+    boolean IS_GPU_ADDED = true;
+    String GPU_TYPE = "NVIDIA V100";
+    int NUMBER_OF_GPUS = 1;
+    String LOCAL_SSD = "2x375 GB";
+    String DATACENTER_LOCATION = "Netherlands (europe-west4)";
+}
