1f95e675ab327104e9dfe8910867add4a4873df5
Steeva06 <steeva0620@gmail.com>
2025-05-06 17:21:19 +0530

    Update TestConstants.java

--- src/main/java/com/epam/training/student_Joseph Steve Waugh_Tummapudi/webdriver_task1/testdata/TestConstants.java
+++ src/main/java/com/epam/training/student_Joseph Steve Waugh_Tummapudi/webdriver_task1/testdata/TestConstants.java
@@ -1,5 +1,5 @@
 
-package com.epam.task1.testdata;
+package com.epam.webdriver_task1.testdata;
 public interface TestConstants {
     String TEXT = "Hello from WebDriver";
     String TITLE = "helloweb";
